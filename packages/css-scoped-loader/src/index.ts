e5dad09c64f643d47c063a93b45ec7bd5050bed7
renzp94 <1074720760@qq.com>
2022-03-15 13:24:14 +0800

    feat: 添加postcss-css-scoped

--- packages/css-scoped-loader/src/index.ts
+++ packages/css-scoped-loader/src/index.ts
@@ -1,5 +1,5 @@
 import postcss from 'postcss'
-import postcssCssScoped from './postcss-css-scoped'
+import postcssCssScoped from '@renzp/postcss-css-scoped'
 
 export default function cssLoader(source) {
   const { css } = postcss([postcssCssScoped(this.resourcePath)]).process(source)
---
+++ packages/postcss-css-scoped/README.md
@@ -0,0 +1,15 @@
+# @renzp/postcss-css-scoped
+
+为`css`文件中添加`scoped`的`postcss plugin`
+
+> 注意：需要搭配`@renzp/jsx-scoped-loader`或`@renzp/babel-plugin-jsx-scoped`一起使用
+
+## Usage
+
+```
+npm init @renzp/postcss-css-scoped
+```
+
+## Options
+
+- `resourcePath`: 当前处理文件的全路径
